607fa78000d155b585d37f1b4d05f125adfa3230
Charl <charl.mert@miranetworks.net>
2012-03-29 16:56:31 +0200

    yeay

--- salsa/depends/bash_log_output_to_stdout_and_file.sh
+++ salsa/depends/bash_log_output_to_stdout_and_file.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo "something happening, yeay, term sing" | tee -a /home/charl/sand/tee_logging.txt
+
+
+preexec () { 
+    echo "trrrrapped $1";
+}
+preexec_invoke_exec () {
+    [ -n "$COMP_LINE" ] && return  # do nothing if completing
+    local this_command=`history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//g"`;
+    preexec "$this_command"
+}
+trap 'preexec_invoke_exec' DEBUG
